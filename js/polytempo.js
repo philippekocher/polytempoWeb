df9f9859fc1244e72de57011322c5453e6f033fa
Adrián Artacho <adrian.artacho@gmail.com>
2022-05-04 12:35:48 +0200

    Added event type playSound()

--- js/polytempo.js
+++ js/polytempo.js
@@ -543,6 +543,10 @@ function executeEvent(event) {
 		
 		else if(event.type == "loadImage") loadImage(event);
 		
+		//else if(event.type == "loadSound") loadSound(event);      // needed to populate an array of soundfiles, same as with images
+        
+        	else if(event.type == "playSound") playSound(event);        // experimental new feature
+		
 		else if(event.type == "addRegion") regions[event.regionID] = event;
 		
 		else if(event.type == "addSection") addSection(event);
